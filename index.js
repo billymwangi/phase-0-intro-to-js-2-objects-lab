8dede0ea2061d279e9df0f13eea29293c9f18bea
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-05 10:51:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const employee = {name: "Billy", streetAddress: "Nairobi" };
+
+function updateEmployeeWithKeyAndValue {...employee, jobTitle: "software engineer" };
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue {
+    ...employee, name: "Paul"
+}
+
+function deleteFromEmployeeByKey {
+
+}
+
+function destructivelyDeleteFromEmployeeByKey {
+
+}
